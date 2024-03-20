fce217dd50ddba6059ff6c9b894c13bd33114d1b
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-03-20 17:38:47 +0100

    feat: Add packages for dealing with imports and update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,10 +20,24 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react', '@react-native-community'],
+  plugins: ['@typescript-eslint', 'react', '@react-native-community', 'unused-imports', 'simple-import-sort'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/no-unescaped-entities': 'off',
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
   globals: {
     it: 'readonly',
