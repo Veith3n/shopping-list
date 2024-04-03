548ea44e4dd3bd88dd121a76cc5c54d90341e624
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-04-03 21:21:04 +0200

    feat: Make props param optional

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -17,10 +17,7 @@ type ThemeProps = {
 export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
 
-export function useThemeColor(
-  props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
+export function useThemeColor(colorName: keyof typeof Colors.light & keyof typeof Colors.dark, props: { light?: string; dark?: string } = {}) {
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
 
@@ -33,14 +30,14 @@ export function useThemeColor(
 
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const color = useThemeColor('text', { light: lightColor, dark: darkColor });
 
   return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor('background', { light: lightColor, dark: darkColor });
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
