e09da73d74086cf0ef24ee6fccda89944e899fd5
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-04-05 23:15:19 +0200

    feat: Display shopping list view as an only screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 import React from 'react';
-import { Pressable } from 'react-native';
 
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useColorScheme } from '@/components/useColorScheme';
@@ -22,41 +21,14 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: { display: 'none' },
       }}
     >
       <Tabs.Screen
-        name="products"
-        options={{
-          title: 'Products tabs',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Products shopping list',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,130 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Button, SectionList, StatusBar, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { DeleteProductAlert } from '@/components/alerts/ProductDeletionAlert';
+import { AddProductForm, AddProductFormValues } from '@/components/forms/AddProductForm';
+import { Product, ProductData, ProductSection, ProductSectionHeader } from '@/components/Product';
+import { TextInput, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+const PRODUCT_LIST: ProductData[] = [
+  {
+    id: 1,
+    name: 'SuperLongPizzaName SuperLongPizzaNameSuperLongPizzaNameSuperLongPizzaNameSuperLongPizzaNameSuperLongPizzaName',
+    price: 10,
+    shopName: 'Pizza Hut',
+  },
+  { id: 2, name: 'Burger', price: 5, shopName: 'McDonalds' },
+  { id: 3, name: 'Risotto', price: 15, shopName: 'Olive Garden' },
+  { id: 4, name: 'French Fries', price: 2, shopName: 'McDonalds' },
+  { id: 5, name: 'Onion Rings', price: 3, shopName: 'Burger King' },
+  { id: 6, name: 'Fried Shrimps', price: 8, shopName: 'Red Lobster' },
+  { id: 7, name: 'Water', price: 1, shopName: 'Publix' },
+  { id: 8, name: 'Coke', price: 2, shopName: 'Publix' },
+  { id: 10, name: 'Beer', price: 5, shopName: 'Publix' },
+  { id: 12, name: 'Cheese Cake', price: 4, shopName: 'Cheesecake Factory' },
+  { id: 22, name: 'Ice Cream', price: 3, shopName: 'Cold Stone' },
+];
+
+export default function ProductsScreen() {
+  const [productList, setProductList] = useState<ProductData[]>(PRODUCT_LIST);
+  const [showAddProductForm, setShowAddProductForm] = useState(false);
+  const [priceFilter, setPriceFilter] = useState<number | null>(null);
+  const [shopFilter, setShopFilter] = useState<string | null>(null);
+
+  const groupedData = productList
+    .filter((product) => priceFilter === null || product.price.toString().includes(priceFilter.toString()))
+    .filter((product) => shopFilter === null || product.shopName.toLowerCase().includes(shopFilter.toLowerCase()))
+    .reduce((acc: ProductSection[], item: ProductData) => {
+      const sectionExists = acc.find((section) => section.title === item.shopName);
+
+      if (sectionExists) {
+        sectionExists.data.push(item);
+      } else {
+        acc.push({ title: item.shopName, data: [item] });
+      }
+
+      return acc;
+    }, []);
+
+  const addProduct = () => {
+    setShowAddProductForm(true);
+  };
+
+  const handleAddProduct = (product: AddProductFormValues) => {
+    const nextId = Math.max(...productList.map((product) => product.id)) + 1;
+    setProductList([{ id: nextId, ...product }, ...productList].map((product) => ({ ...product })));
+
+    setShowAddProductForm(false);
+  };
+
+  const handleDeleteProduct = (productToDelete: ProductData) => {
+    const handleDeleteProduct = () => {
+      setProductList((prevList) => {
+        const indexToRemove = prevList.findIndex((product) => product.id === productToDelete.id);
+
+        if (indexToRemove === -1) {
+          return prevList;
+        }
+
+        return [...prevList.slice(0, indexToRemove), ...prevList.slice(indexToRemove + 1)];
+      });
+    };
+
+    DeleteProductAlert({ productName: productToDelete.name, onDelete: handleDeleteProduct });
+  };
+
+  const handleMarkAsPurchased = (productToMark: ProductData) => {
+    setProductList((prevList) => {
+      const indexToMark = prevList.findIndex((product) => product.id === productToMark.id);
+
+      if (indexToMark === -1) {
+        return prevList;
+      }
+
+      const updatedProduct = { ...prevList[indexToMark] };
+      updatedProduct.purchased = true;
+
+      return [...prevList.slice(0, indexToMark), updatedProduct, ...prevList.slice(indexToMark + 1)];
+    });
+  };
+
+  const handlePriceFilterChange = (price: string) => {
+    const parsedPrice = parseInt(price);
+
+    const numericPrice = Number.isNaN(parsedPrice) ? null : parsedPrice;
+
+    setPriceFilter(numericPrice);
+  };
+
+  const handleShopFilterChange = (shop: string) => {
+    setShopFilter(shop);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {!showAddProductForm && <Button title="Add Product" onPress={addProduct} />}
+      {showAddProductForm && <AddProductForm onAddProduct={handleAddProduct} />}
+      <View style={styles.filterContainer}>
+        <View style={styles.filterItem}>
+          <TextInput
+            style={styles.filterInput}
+            placeholder="Filter by price"
+            keyboardType="numeric"
+            onChangeText={handlePriceFilterChange}
+            value={priceFilter ? priceFilter.toString() : ''}
+          />
+        </View>
+        <View style={styles.filterItem}>
+          <TextInput style={styles.filterInput} placeholder="Filter by shop" onChangeText={handleShopFilterChange} value={shopFilter ?? ''} />
+        </View>
+      </View>
+
+      <SectionList
+        sections={groupedData}
+        keyExtractor={(item, index) => item.name + index}
+        renderItem={({ item }) => Product({ item, styles: styles.item, onRemoval: handleDeleteProduct, onMarkAsPurchased: handleMarkAsPurchased })}
+        renderSectionHeader={({ section }) => ProductSectionHeader(section, styles.header)}
+      />
     </View>
   );
 }
@@ -16,16 +132,34 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    padding: 10,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    padding: 20,
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 24,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  filterContainer: {
+    paddingTop: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  filterItem: {
+    flex: 1,
+    marginHorizontal: 8,
+  },
+  filterInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    padding: 8,
   },
 });
