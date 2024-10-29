8896edf49586a999414e65b49e3cc3ad946f71ce
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-10-29 21:42:55 +0100

    feat: Add open meteo api types

---
+++ common/api/open-meteo-api.types.ts
@@ -0,0 +1,45 @@
+export interface OpenMeteoApiService {
+  getCurrentWeatherData(): Promise<GetCurrentWeatherDataResponse>;
+}
+
+export type GetCurrentWeatherDataResponse = WeatherApiCurrentWeatherResponse | FailedResponse;
+
+export interface FailedResponse {
+  reason: string;
+  error: true;
+}
+
+export interface WeatherApiBaseResponse {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+}
+
+export interface WeatherApiCurrentWeatherResponse extends WeatherApiBaseResponse {
+  current_weather_units: CurrentWeatherUnits;
+  current_weather: CurrentWeather;
+}
+
+export interface CurrentWeatherUnits {
+  time: string;
+  interval: string;
+  temperature: string;
+  windspeed: string;
+  winddirection: string;
+  is_day: string;
+  weathercode: string;
+}
+
+export interface CurrentWeather {
+  time: string;
+  interval: number;
+  temperature: number;
+  windspeed: number;
+  winddirection: number;
+  is_day: number;
+  weathercode: number;
+}
